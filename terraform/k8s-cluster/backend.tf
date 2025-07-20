970acb9c382a5c723114a256570b526386788546
evandeters <evanjd711@gmail.com>
2025-07-19 23:38:18 -0700

    test

--- terraform/k8s-cluster/backend.tf
+++ terraform/k8s-cluster/backend.tf
@@ -1,4 +1,12 @@
 terraform {
     backend "http" {
+      address        = "${CI_API_V4_URL}/projects/${CI_PROJECT_ID}/terraform/state/${CI_COMMIT_REF_NAME}"
+      lock_address   = "${CI_API_V4_URL}/projects/${CI_PROJECT_ID}/terraform/state/${CI_COMMIT_REF_NAME}/lock"
+      unlock_address = "${CI_API_V4_URL}/projects/${CI_PROJECT_ID}/terraform/state/${CI_COMMIT_REF_NAME}/lock"
+      username       = "${CI_USERNAME}"
+      password       = "${CI_JOB_TOKEN}"
+      lock_method    = "POST"
+      unlock_method  = "DELETE"
+      retry_wait_min = 5
     }
 }
